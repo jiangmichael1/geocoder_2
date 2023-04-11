e8252d1e329a3324f96cd6d45890398d67fa5e22
jiangmichael1 <jiangm1@yahoo.com>
2023-04-11 19:27:14 -0400

    created excel container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import './App.css';
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    file: null,
+    header: [],
+    body: [],
+    results: [],
+    status: {
+      start: false,
+      count: 0,
+      resultsCount: 0,
+      errorsCount: 0,
+      finshed: false,
+    },
+    tabOptions: [],
+    tabValue: 0,
+    fileName : '',
+    fileError: false,
+    exportColumns: {},
+    isEditorOpen: false,
+    currentEdit: {}
+  }
+
   render()
   {
     return (
